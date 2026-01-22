483ac3a8edf33736d15b33f1c97501e9c029534e
Everson Silva <everson.3397@gmail.com>
2026-01-22 14:33:07 +0100

    VOTAR-14: fix .env file and app route

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
+import "../config";
 import chatRoute from "./routes/chat.routes";
-
-dotenv.config();
+import appRoute from "./routes/app.routes";
 
 const commonMiddleware = [
   cors({
@@ -17,6 +16,7 @@ const app = express();
 app.use(commonMiddleware);
 
 // Routes
+app.use("/", appRoute)
 app.use("/api/chat", chatRoute);
 
 export default app;
--- src/routes/chat.routes.ts
+++ src/routes/chat.routes.ts
@@ -5,8 +5,4 @@ const router = Router();
 
 router.post("/generate-stream", generateStream);
 
-router.get("/", (_, res) => {
-  res.status(200).send("VotarIA Server is running");
-});
-
 export default router;
