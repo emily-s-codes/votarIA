b63e7a6cb8f8f4aaddabc87a8210f73d520b382d
Emily Sheil <sheil.emily.sc@gmail.com>
2026-01-30 11:38:05 +0100

    update component name for test

--- votarIA-app/src/app/sidebar/sidebar.component.spec.ts
+++ votarIA-app/src/app/sidebar/sidebar.component.spec.ts
@@ -1,18 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AppSidebar } from './sidebar.component';
+import { SidebarComponent } from './sidebar.component';
 
-describe('AppSidebar', () => {
-  let component: AppSidebar;
-  let fixture: ComponentFixture<AppSidebar>;
-
+describe('SidebarComponent', () => {
+  let component: SidebarComponent;
+  let fixture: ComponentFixture<SidebarComponent>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppSidebar]
+      imports: [SidebarComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(AppSidebar);
+    fixture = TestBed.createComponent(SidebarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
