62e27b690266c04dbdedd6bb0d3c7f1f1d39e58e
Everson Silva <everson.3397@gmail.com>
2026-02-24 19:31:23 +0100

    VOTAR-47: Deploy application to GCP (backend)

--- votar-ia/src/main.ts
+++ votar-ia/src/main.ts
@@ -12,6 +12,9 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
   
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 8080;
+  await app.listen(port, '0.0.0.0'); 
+  
+  console.log(`Backend running on port: ${port}`);
 }
 bootstrap();
